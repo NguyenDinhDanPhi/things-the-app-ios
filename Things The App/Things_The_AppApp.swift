9ab302d3277337b4035c86f25f45e74d97f7a569
danphi <phittnh@gmail.com>
2023-07-20 10:01:10 +0700

    Initial Commit

---
+++ Things The App/Things_The_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Things_The_AppApp.swift
+//  Things The App
+//
+//  Created by dan phi on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Things_The_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
